ed23b95986ecb361e195d44275c9324175c08399
Techbotlover <Manishraj0491@gmail.com>
2024-06-18 17:45:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ API_ID = API_ID = 27560342
 
 API_HASH = os.environ.get("API_HASH", "cd10f0b290f53f7c9a3f22c6cd788b60")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7291812440:AAHudgxXyYigFk2TF1jfCrezLCI1jZYDJFk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7413157986:AAEuSx4-9vgIqDoiutuXCoddD1mzYROk7_w")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6988460039))
+OWNER = int(os.environ.get("OWNER", 5648658139))
 
 LOG = -1002054181760
 
 try:
-    ADMINS=[7045457279]
+    ADMINS=[6988460039]
     for x in (os.environ.get("ADMINS", "6988460039").split()):
         ADMINS.append(int(x))
 except ValueError:
