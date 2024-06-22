720ca5b54202b1136d5e8e2f2618ec297d2f65a5
Techbotlover <Manishraj0491@gmail.com>
2024-06-22 08:30:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_ID = API_ID = 27560342
 
 API_HASH = os.environ.get("API_HASH", "cd10f0b290f53f7c9a3f22c6cd788b60")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7413157986:AAEuSx4-9vgIqDoiutuXCoddD1mzYROk7_w")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7036857870:AAEggZOads3-E0rUIHgsyIcaymBN4w1uN54")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 5648658139))
+OWNER = int(os.environ.get("OWNER", 5656575955))
 
 LOG = -1002054181760
 
